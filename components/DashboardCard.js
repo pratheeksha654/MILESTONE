d820b67ac7da2cc312ce374124a66849974d4644
PrajnaKV123 <prajnakv584@gmail.com>
2026-02-10 23:17:55 +0530

    final outcome

---
+++ components/DashboardCard.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function DashboardCard({
+  title,
+  description,
+  link,
+  linkText,
+}) {
+  return (
+    <div className="p-6 rounded-2xl shadow transition hover:-translate-y-1 hover:shadow-xl bg-white dark:bg-gray-800
+     text-blue-700">
+      <h2 className="text-xl font-semibold">{title}</h2>
+
+      <p className="mt-2 text-gray-600 dark:text-gray-300">
+        {description}
+      </p>
+
+      {link && linkText && (
+        <Link
+          href={link}
+          className="mt-4 inline-block text-sm font-medium text-blue-600 dark:text-blue-400 hover:underline"
+        >
+          {linkText}
+        </Link>
+      )}
+    </div>
+  );
+}
